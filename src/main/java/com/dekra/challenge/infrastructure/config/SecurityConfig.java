807b52ae1d80f4d040bbe58856fea7fb698604c4
EuroCase007 <nicolaspollio97@gmail.com>
2026-02-15 23:54:13 +0100

    delete unneeded import and add postman collection

--- src/main/java/com/dekra/challenge/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/dekra/challenge/infrastructure/config/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.dekra.challenge.infrastructure.config;
 
-import com.dekra.challenge.domain.security.modelo.UserRole;
 import com.dekra.challenge.infrastructure.security.jwt.JWTFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
